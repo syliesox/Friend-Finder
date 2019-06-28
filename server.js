961d276a34bea0bde062a78b29b9d3dea53086af
syliesox <syliesox@gmail.com>
2019-06-28 15:28:35 -0400

    added server info and connection to server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+// Required NPM Packages for Dependencies
+var express = require("express");
+var path = require("path");
+
+// Set up of the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Set up of the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routing Info
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
